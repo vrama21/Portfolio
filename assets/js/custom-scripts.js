9dd5600004ed6427b452f076b54eb0d144823736
Valon Rama <vrama21@gmail.com>
2020-02-08 16:48:55 -0500

    Minor touchups

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -16,20 +16,6 @@
                 queue: true
             }
         });
-
-        $('.portfolio-nav li').click(function () {
-            $('.portfolio-nav .current').removeClass('current');
-            $(this).addClass('current');
-
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    queue: true
-                }
-            });
-            return false;
-        });
     });
     /* Loader Code End */
 
